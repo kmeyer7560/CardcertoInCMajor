2b4f6a31abda1ad3a0c7a2d183ad31ee6f639821
HugoB <hberneis9830@student.pps.net>
2024-11-13 13:45:10 -0800

    first boss movement and chicken attack

--- Assets/Scripts/BossScripts/BossStats.cs
+++ Assets/Scripts/BossScripts/BossStats.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
 public class BossStats : MonoBehaviour
@@ -9,18 +11,31 @@ public class BossStats : MonoBehaviour
     public BossHealthBar healthBar;
     public bool tractorBoss;
     private float tractorSpeed = 5f;
+    public GameObject chicken;
+    public Transform chickenPos;
+    public bool drive;
+    Vector3 lastVelocity;
 
     public GameObject player;
     private Transform playerTarget;
     Animator anim;
+    public float movespeed = 10f;
+    private Rigidbody2D rb;
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+
         currentHealth = maxHealth;
         anim = GetComponent<Animator>();
 
         healthBar.SetSliderMax(maxHealth);
         player = GameObject.FindGameObjectWithTag("Player");
         playerTarget = player.GetComponent<Transform>();
+    }
+    public void Drive()
+    {
+        rb.AddForce(transform.up * 1000f);
+        rb.AddForce(-transform.right * 800f);
     }
 
     public void Attack(float amount)
@@ -39,8 +54,18 @@ public class BossStats : MonoBehaviour
         currentHealth += amount;
         healthBar.SetSlider(currentHealth);
     }
+    public void ChickenAttack()
+    {
+        //for(int i=0;i<=10 i++; )
+        //Instantiate(chicken, chickenPos.position, Quaternion.identity);
+    }
     private void Update()
     {
+        lastVelocity = rb.velocity;
+        if(drive)
+        {
+            Drive();
+        }
         if(currentHealth <= maxHealth*.5)
         {
             anim.SetTrigger("TractorPhaseTwo");
@@ -49,5 +74,13 @@ public class BossStats : MonoBehaviour
         {
             anim.SetTrigger("TractorBossDeath");
         }
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        var speed = lastVelocity.magnitude;
+        var direction = Vector3.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0f);
     }
 }
