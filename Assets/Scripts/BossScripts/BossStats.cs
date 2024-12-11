7f6225965edbadfcfc288be9ea9fde3a3f459373
Hugo Berneis <hberneis9830@student.pps.net>
2024-12-10 18:09:48 -0800

    remade the boss driving script. it works!

--- Assets/Scripts/BossScripts/BossStats.cs
+++ Assets/Scripts/BossScripts/BossStats.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEditor.Callbacks;
 using UnityEngine;
@@ -11,95 +10,87 @@ public class BossStats : MonoBehaviour
     private float currentHealth;
     public BossHealthBar healthBar;
     public bool tractorBoss;
-    private float tractorSpeed = 5f;
     public GameObject chicken;
     public Transform chickenPos;
-    public bool drive;
-    Vector3 lastVelocity;
 
     public GameObject player;
     private Transform playerTarget;
     Animator anim;
-    public float movespeed = 10f;
-    private Rigidbody2D rb;
-    public Transform BossSprite;
+
     private int chickens = 10;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerTarget = player.transform;
+        }
+    }
+
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        drive = false;
         currentHealth = maxHealth;
-        anim = GetComponent<Animator>();
 
-        healthBar.SetSliderMax(maxHealth);
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerTarget = player.GetComponent<Transform>();
+        if (healthBar != null)
+        {
+            healthBar.SetSliderMax(maxHealth);
+        }
     }
-    public void DriveLeft()
-    {
-        rb.AddForce(transform.up * 700f);
-        rb.AddForce(-transform.right * 500f);
-        Invoke("DriveRight", 1f);
-    }
-    public void DriveRight()
-    {
-        rb.AddForce(-transform.up * 700f);
-        rb.AddForce(transform.right * 500f);
-        Invoke("DriveLeft", 1f);
-    }
-    public void Attack(float amount)
-    {
-        //attack player
-    }
-    public void TakeDamage(float amount)
-    {
-        //subtracts the health by the amount inputed when function is called and updates slider
-        currentHealth -= amount;
-        healthBar.SetSlider (currentHealth);
-    }
-    public void Heal(float amount)
-    {
-        //adds the health by the amount inputed when function is called and updates slider
-        currentHealth += amount;
-        healthBar.SetSlider(currentHealth);
-    }
-    public void ChickenAttack()
-    {
-        if(chickens>0)
-        Instantiate(chicken, chickenPos.position, Quaternion.identity);
-        chickens--;
-        Invoke("ChickenAttack",.1f);
-    }
+
     void Update()
     {
-        if(Input.GetKeyDown("p"))
+        if (Input.GetKeyDown("p"))
         {
             TakeDamage(100);
         }
-        lastVelocity = rb.velocity;
-        if(drive)
+        if (currentHealth <= maxHealth * 0.5f)
         {
-            DriveLeft();
+            if (anim != null)
+            {
+                anim.SetTrigger("TractorPhaseTwo");
+            }
         }
-        if(currentHealth <= maxHealth*.5)
+        if (currentHealth <= 0)
         {
-            anim.SetTrigger("TractorPhaseTwo");
+            if (anim != null)
+            {
+                anim.SetTrigger("TractorBossDeath");
+            }
         }
-        if(currentHealth <= 0)
-        {
-            anim.SetTrigger("TractorBossDeath");
-        }
-    }
-    public void StartChase(){
-        Debug.Log("startchase");
-        drive = true;
     }
 
-    private void OnCollisionEnter(Collision coll)
+    public void Attack(float amount)
     {
-        var speed = lastVelocity.magnitude;
-        var direction = Vector3.Reflect(lastVelocity.normalized, coll.contacts[0].normal);
+        // Attack player logic here
+    }
 
-        rb.velocity = direction * Mathf.Max(speed, 0f);
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (healthBar != null)
+        {
+            healthBar.SetSlider(currentHealth);
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        if (healthBar != null)
+        {
+            healthBar.SetSlider(currentHealth);
+        }
+    }
+
+    public void ChickenAttack()
+    {
+        if (chickens > 0 && chicken != null && chickenPos != null)
+        {
+            Instantiate(chicken, chickenPos.position, Quaternion.identity);
+            chickens--;
+            Invoke("ChickenAttack", 0.1f);
+        }
     }
 }
---
+++ Assets/Scripts/BossScripts/CrazyDriving.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+
+public class CrazyDriving : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float reverseSpeed = 3f;
+    public float rotationSpeed = 100f;
+    public float visionRange = 10f;
+    public float ramSpeed = 15f;
+    public float reverseTime = 2f;
+    public LayerMask playerLayer;
+    public LayerMask wallLayer;
+
+    private Rigidbody2D rb;
+    private Transform player;
+    private bool isRamming = false;
+    private bool isReversing = false;
+    private Vector2 lastKnownPlayerPosition;
+    private Vector2 ramDirection;
+    private float reverseTimer;
+     private bool rotateClockwise;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void FixedUpdate()
+    {
+        if (isReversing)
+        {
+            ReverseFromWall();
+        }
+        else if (isRamming)
+        {
+            RamTowardsPlayer();
+        }
+        else if (CanSeePlayer())
+        {
+            PrepareToRam();
+        }
+        else
+        {
+            SearchForPlayer();
+        }
+    }
+
+    bool CanSeePlayer()
+    {
+        Debug.Log("Boss is checking for player");
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, visionRange, playerLayer);
+        if (hit.collider != null)
+        {
+            Debug.Log("Boss has spotted player");
+            lastKnownPlayerPosition = hit.point;
+            return true;
+        }
+        return false;
+    }
+
+    void PrepareToRam()
+    {
+        Debug.Log("Boss is preparing to ram");
+        Vector2 directionToPlayer = (lastKnownPlayerPosition - (Vector2)transform.position).normalized;
+        float angle = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = Mathf.LerpAngle(rb.rotation, angle, rotationSpeed * Time.fixedDeltaTime);
+
+        if (Mathf.Abs(Mathf.DeltaAngle(rb.rotation, angle)) < 5f)
+        {
+            isRamming = true;
+            ramDirection = directionToPlayer;
+        }
+    }
+
+    void RamTowardsPlayer()
+    {
+        Debug.Log("Boss is ramming");
+        rb.velocity = ramDirection * ramSpeed;
+    }
+
+    void ReverseFromWall()
+    {
+        Debug.Log("Boss is reversing");
+        rb.velocity = -transform.up * reverseSpeed;
+        reverseTimer -= Time.fixedDeltaTime;
+        if (reverseTimer <= 0)
+        {
+            isReversing = false;
+            isRamming = false;
+        }
+    }
+
+    void SearchForPlayer()
+    {
+        Debug.Log("Boss is searching");
+        rb.velocity = transform.up * moveSpeed;
+        float rotationDirection = rotateClockwise ? -1 : 1;
+        rb.angularVelocity = rotationSpeed * rotationDirection;
+        if (Random.value < 0.02f)
+        {
+            rotateClockwise = !rotateClockwise;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("ArenaWall"))
+        {
+            Debug.Log("Boss hit a wall");
+            isRamming = false;
+            isReversing = true;
+            reverseTimer = reverseTime;
+        }
+    }
+}
