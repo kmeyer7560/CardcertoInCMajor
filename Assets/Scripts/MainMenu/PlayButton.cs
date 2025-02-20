ae35cf4736bd51d0cc139ddc485742d47e0c4e56
HugoB <hberneis9830@student.pps.net>
2025-02-20 14:01:38 -0800

    added playbutton code

--- Assets/Scripts/MainMenu/PlayButton.cs
+++ Assets/Scripts/MainMenu/PlayButton.cs
@@ -2,11 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class PlayButton : MonoBehaviour
 {
+    public Transform mainMenu;
     public void StartGame()
     {
+        Debug.Log("start game");
         SceneManager.LoadScene("ForestMain");
+        mainMenu = GameObject.Find("MainImage").transform;
+    }
+
+    public void SettingsCameraPosition()
+    {
+        mainMenu.transform.position = Vector3.MoveTowards(mainMenu.transform.position, new Vector3(1f, 1f, 1f), 2*Time.deltaTime);
+    }
+
+    public void MainCameraPosition()
+    {
+        mainMenu.transform.position = Vector3.MoveTowards(mainMenu.transform.position, new Vector3(3f, 3f, 3f), 2* Time.deltaTime);
     }
 }
